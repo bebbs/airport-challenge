6924ebffcf6d031d12378336a1c74fbb44b4537b
Josh Bebbington <josh@focalwebsites.com>
2014-12-12 14:15:22 +0000

    Implemented dispatch and landing methods for airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,4 +1,17 @@
 class Airport
 
+  def planes
+    @planes ||= []
+  end
+
+  def land_plane(plane)
+    planes << plane
+    plane.land
+  end
+
+  def dispatch_plane(plane)
+    planes.delete(plane)
+    plane.take_off
+  end
 
 end
